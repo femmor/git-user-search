8e0ab6073d52f2c5147f3328e5d9c3f48b745c21
Emmanuel Egomson <fegomson@gmail.com>
2021-06-21 19:26:23 -0500

    Created the Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
+import {FaRegSadTear} from "react-icons/fa"
 import { Link } from 'react-router-dom';
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      
+      <h1>
+      <FaRegSadTear/> Oops!
+      </h1>
+      <h3>sorry, the page can not be found</h3>
+      <Link to="/" className="btn">
+        back home
+      </Link>
+    </Wrapper>
+  )
 };
+
+// Styles
 const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
   min-height: 100vh;
-  display: grid;
-  place-items: center;
+  justify-content: center;
+  align-items: center;
   background: var(--clr-primary-10);
-  text-align: center;
   h1 {
     font-size: 10rem;
+    margin-bottom: 5rem;
   }
   h3 {
     color: var(--clr-grey-3);

e484b1ec73ebc208d7a6d2924d7bcfb4facefee4
Emmanuel Egomson <fegomson@gmail.com>
2021-06-22 15:05:08 -0500

    Created the Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,46 @@ import { useGithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const {githubUser} = useGithubContext()
+  const {
+    id, 
+    avatar_url, 
+    html_url, 
+    name, 
+    company, 
+    blog, 
+    bio, 
+    location, 
+    twitter_username
+  } = githubUser
+
+  return(
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || "john_doe"}</p>
+        </div>
+        <a href={html_url} target="_blank" rel="noreferrer">follow</a>
+      </header>
+      <p className="bio">{bio}</p>
+      <div className="links">
+        <p>
+          <MdBusiness/> {company || 'Web Developer'}
+        </p>
+        <p>
+          <MdLocationOn/> {location || 'Earth'}
+        </p>
+        <p>
+          <MdLink/> 
+          <a href={blog || '#'} target="_blank" rel="noreferrer">{blog || 'No blog'}</a>
+        </p>
+      </div>
+    </Wrapper>
+  );
 };
+
 const Wrapper = styled.article`
   background: var(--clr-white);
   padding: 1.5rem 2rem;

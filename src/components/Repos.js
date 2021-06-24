da3426566268988dea22308d6f489df3a1077405
Emmanuel Egomson <fegomson@gmail.com>
2021-06-24 14:42:50 -0500

    Calculate most used language

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,28 +4,55 @@ import { useGithubContext } from '../context/context';
 import { Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const {repos} = useGithubContext()
-  console.log(repos);
-
-  const chartData = [
-    {
-      label: "HTML",
-      value: "13"
-    },
-    {
-      label: "CSS",
-      value: "23"
-    },
-    {
-      label: "JavaScript",
-      value: "80"
-    },
+  
+  // Use reduce method to get the % of Language
+  let languages = repos.reduce((total, item) => {
+    // Destructure language out of each repo - repo is an object
+    const {language} = item
+    // check if language is null
+    if (!language) {
+      // return it
+      return total
+    } 
+    // Check if the returned total object has the language property
+    if (!total[language]) {
+      // if it does not, create one - total[language]
+      total[language] = { label: language, value: 1 }
+    } else {
+      // if it does, if the language is there, keep the language property, and override the value of the language property
+      total[language] = { ...total[language], value: total[language].value + 1 }
+    }
     
-  ];
+    // return total
+    return total
+  }, {})
+  
+  // Since we are setting it as an array, we use Object.values() method
+  languages = Object.values(languages)
+    // Sort it based on highest values first
+    .sort((a, b) => b.value - a.value)
+    // Get the first 5 values
+    .slice(0, 5)
+
+  // const chartData = [
+  //   {
+  //     label: "HTML",
+  //     value: "13"
+  //   },
+  //   {
+  //     label: "CSS",
+  //     value: "23"
+  //   },
+  //   {
+  //     label: "JavaScript",
+  //     value: "80"
+  //   },
+  // ];
 
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <Pie3D data={chartData}/>
+        <Pie3D data={languages}/>
       </Wrapper>
     </section>
   )
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,7 +4,6 @@ import { useGithubContext } from '../context/context';
 import loadingImage from '../images/preloader.gif';
 const Dashboard = () => {
   const data = useGithubContext()
-  console.log(data);
 
   return (
     <main>
